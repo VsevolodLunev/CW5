9b89b6345fd1767d759115b63d0e223b3d7f1804
VsevolodLunev <Sevalunev@list.ru>
2024-09-04 16:07:59 +0300

    pretest commit

--- src/create_db.py
+++ src/create_db.py
@@ -1,8 +1,11 @@
+
 import psycopg2
 
 
 def create_data_base(database_name, params) -> None:
-    """Создание базы данных и таблиц с данными о компаниях и вакансиях"""
+    """
+    Создание базы данных и таблиц с данными о компаниях и вакансиях
+    """
 
     conn = psycopg2.connect(dbname='postgres', **params)
     conn.autocommit = True
@@ -21,41 +24,39 @@ def create_data_base(database_name, params) -> None:
             CREATE TABLE IF NOT EXISTS companies (
             company_id int,
             company_name VARCHAR(255),
-            company_url TEXT,
-            
-            CONSTRAINT pk_companies_company_name PRIMARY KEY (company_name));
+            company_url TEXT
             )
             """)
 
-            with conn.cursor() as cur:
-                cur.execute("""
-                CREATE TABLE IF NOT EXISTS vacancies (
-                company_name VARCHAR(255),
-                job_title VARCHAR,
-                link_to_vacancy TEXT,
-                salary_from int,
-                currency VARCHAR(20),
-                experience TEXT,
-                description TEXT,
-                requirement TEXT
-                )
-                ALTER TABLE vacancies ADD CONSTRAINT fk_vacancies_companies FOREIGN KEY(company_name)
-                REFERENCES companies(company_name)
-                """)
+        with conn.cursor() as cur:
+            cur.execute("""
+            CREATE TABLE IF NOT EXISTS vacancies (
+            company_name VARCHAR(255),
+            job_title VARCHAR,
+            link_to_vacancy TEXT,
+            salary_from int,
+            currency VARCHAR(20),
+            experience TEXT,
+            description TEXT,
+            requirement TEXT
+            )
+            """)
     conn.commit()
     conn.close()
 
 
 def save_data_to_db(data, database_name, params) -> None:
-    """Заполнение таблиц данными"""
+    """
+    Заполнение таблиц данными
+    """
     insert_q = """
         INSERT INTO vacancies (company_name, job_title, link_to_vacancy, salary_from, currency, experience,description,
         requirement)
         VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
         """
     insert_q_2 = """
-        INSERT INTO companies (company_id, company_name, company_url) VALUSES (%s, %s, %s)
-        """
+                 INSERT INTO companies (company_id, company_name, company_url) VALUSES (%s, %s, %s)
+                 """
     with psycopg2.connect(dbname=database_name, **params) as conn:
         with conn.cursor() as cur:
             for vacancy in data:
